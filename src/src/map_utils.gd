5d3ace4356f4e2583d6ca68273e6216af503f6ae
Vladimir Nani <vladimirnani@gmail.com>
2025-09-21 15:20:14 +0200

    change locations

--- src/src/map_utils.gd
+++ src/src/map_utils.gd
@@ -11,7 +11,7 @@ const GRID_STEP = 5.0
 
 
 #static var start = Vector2(52.372099, 4.892923)
-static var start = Vector2(52.075836, 4.287392)
+static var start = Vector2(52.078247, 4.291105)
 
 # Scale factor to convert degrees to local units
 const SCALE_FACTOR = 200
